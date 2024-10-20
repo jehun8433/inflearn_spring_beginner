05dd4dd4980cc6c3b2f5c37130f601c69ebd774b
jehun.lee <jehun8433@naver.com>
2024-10-20 23:16:04 +0900

    색션 4-4까지 완료

---
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -0,0 +1,47 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member){
+
+
+        // 같은 이름이 있는 중복 회원X
+        //memberRepository.findByName(member.getName()); // 여기서 ctrl+alt+v 누르면 아래와 같이 result가 return형에 맞게 자둥생성
+        //Optional<Member> result = memberRepository.findByName(member.getName());
+
+        validateDuplicateMember(member); // 중복 회원 검증.
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
---
+++ src/test/java/hello/hello_spring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,69 @@
+package hello.hello_spring.repository;
+
+import hello.hello_spring.domain.Member;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+class MemoryMemberRepositoryTest {
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    // 테스트 끝날 때마다 불리도록 셋팅함
+    @AfterEach
+    public void afterEach(){
+        repository.clearStore();
+    }
+
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+
+        //Assertions.assertEquals(member, result);
+        assertThat(member).isEqualTo(result);
+
+    }
+    @Test
+    public void findByName(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member(); // Shift+F6 누르면 동일 변수 한번에 바꾸는 기능 있음 ㅎㅎ
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+
+        assertThat(result).isEqualTo(member1);
+
+    }
+
+    @Test
+    public void findAll(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        System.out.println("result = " + result.get(1).getName());
+        assertThat(result.size()).isEqualTo(2);
+
+    }
+}
