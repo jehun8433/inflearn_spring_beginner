62d50e3b3c80010f0396939d7e4bb06b4c598d9c
jehun.lee <jehun8433@naver.com>
2024-10-31 00:46:36 +0900

    색션 8-1까지 완료

--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -5,11 +5,13 @@ import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 // 테스트 만드는 단축키: Ctrl + Shift + T
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -21,17 +23,25 @@ public class MemberService {
     /**
      * 회원 가입
      */
+
     public Long join(Member member){
-
-
         // 같은 이름이 있는 중복 회원X
         //memberRepository.findByName(member.getName()); // 여기서 ctrl+alt+v 누르면 아래와 같이 result가 return형에 맞게 자둥생성
         //Optional<Member> result = memberRepository.findByName(member.getName());
 
-        validateDuplicateMember(member); // 중복 회원 검증.
+        long start = System.currentTimeMillis();
 
-        memberRepository.save(member);
-        return member.getId();
+        try{
+            validateDuplicateMember(member); // 중복 회원 검증.
+
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
+
     }
 
     private void validateDuplicateMember(Member member) {
@@ -45,7 +55,15 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers(){
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+        try{
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers = " + timeMs + "ms");
+        }
+
     }
 
     public Optional<Member> findOne(Long memberId){
